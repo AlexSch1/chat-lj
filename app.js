62536b659ee22a247764f5dd426c414515feb337
Aliaksei <aleksejsch@yandex.by>
2021-03-06 17:25:52 +0300

    fixed middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,10 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
+if (app.get('env') === 'development') {
+  app.use(logger('dev')); // immediate до запроса или после логировать
+} else {
+  app.use(logger('default'));
+}
+app.use(express.json()); // req.body app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); // req.cookies
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
