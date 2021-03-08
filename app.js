b3010de01405785761adfb49c4f6c21cd0d36d9b
Aliaksei <aleksejsch@yandex.by>
2021-03-08 15:49:32 +0300

    added vendor fixed tmpls

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.engine('ejs', require('ejs-locals'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
