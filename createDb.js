3392fd1b46f592194f3b25eff269f95800fce71f
AlexSch1 <aleksejSch@yandex.by>
2021-03-27 22:33:37 +0300

    create sript for text db 1

--- createDb.js
+++ createDb.js
@@ -26,12 +26,10 @@
 // }
 // run().catch(console.dir);
 
-
 // mongoose
 
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/chat', {useNewUrlParser: true, useUnifiedTopology: true});
-
 
 // const schema = mongoose.Schema({
 //     name: String
@@ -48,11 +46,95 @@
 //     // console.log('meow')
 // });
 
-const User = require('./models/User').User;
+// const User = require('./models/User').User;
 
-const user = new User({
-    username: 'Alex',
-    password: 'alex'
-});
+// const user = new User({
+//     username: 'Alex',
+//     password: 'alex'
+// });
 
-user.save();
+// user.save();
+
+/**
+ * 1 drop database
+ * 2 create & save 3 users
+ * 3 close connection
+ */
+
+ 
+const mongoose = require("./libs/mongoose");
+const User = require("./models/User").User;
+const async = require('async');
+
+// async.series([
+//     open,
+//     dropDB,
+//     createUsers,
+//     close,
+// ], function(err, result) {
+//     // console.log(arguments);
+// });
+
+
+function runner(deeds) {
+    return deeds.reduce(function(p, deed) {
+        return p.then(function() {
+            return deed()
+        })
+    }, Promise.resolve())
+}
+
+function a1() {
+    return new Promise(function(resolve) {
+        setTimeout(() => {
+            console.log('a1');
+            resolve()
+        }, 2000)
+    })
+}
+
+function a2() {
+    return new Promise(function(resolve) {
+        setTimeout(() => {
+            console.log('a2');
+            resolve()
+        }, 200)
+    })
+}
+
+runner([a1, a2]).then((dd) => {
+    console.log('res', dd);
+})
+
+
+function open(cb) {
+  mongoose.connection.on("open", cb);
+}
+
+function close() {
+    mongoose.disconnect();
+}
+
+function dropDB(cb) {
+  const db = mongoose.connection.db;
+  db.dropDatabase(cb);
+}
+
+function createUsers(cb) {
+  const user1 = new User({
+    username: "Alex1",
+    password: "alex1",
+  });
+
+  const user2 = new User({
+    username: "Alex2",
+    password: "alex2",
+  });
+
+  const user3 = new User({
+    username: "Alex3",
+    password: "alex3",
+  });
+
+  Promise.all([user1.save(), user2.save(), user3.save()]).then(cb);
+}
